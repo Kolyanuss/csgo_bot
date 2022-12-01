9d38effd146bf5fb3a16173ac4fa1a0c93dcc898
0xKolyanus <mukola.1zt.@gmail.com>
2022-12-01 22:39:16 +0200

    update creater_dataset script

--- src/creater_dataset.py
+++ src/creater_dataset.py
@@ -2,20 +2,24 @@ import pyautogui
 import cv2
 import numpy as np
 import time
+from pynput import keyboard
 
 path_to_save = "temp_img"
 
-while True:
+def foo():
     screen = pyautogui.screenshot()
     screen_arr = np.array(screen)
-    screen_arr = screen_arr[:1024, 960:, :]
+    screen_arr = screen_arr[27:890, 768:, :]
     screen_arr = cv2.cvtColor(screen_arr, cv2.COLOR_RGB2BGR)
-    # cv2.imshow("game capture", [screen_arr])
+    cv2.imwrite(path_to_save+"/screen_" + str(time.time()) + ".png", screen_arr)
 
-    if cv2.waitKey(1) & 0xFF == ord('c'):
-        cv2.imwrite(path_to_save+"/screen_" + str(time.time()) + ".png", screen_arr)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+def checker(key):
+    # print(f'{key} released')
+    if key == keyboard.Key.alt_l:
+        foo()
+    if key == keyboard.Key.esc:
+        return False
 
-cv2.destroyAllWindows()
+with keyboard.Listener(on_press=checker) as listener:
+    listener.join()
