e80e2f695891a88f13345ee2ff579af405f4ffdc
0xKolyanus <mukola.1zt.@gmail.com>
2022-12-01 22:55:29 +0200

    small update game cap

--- src/gamecap.py
+++ src/gamecap.py
@@ -1,12 +1,18 @@
 import pyautogui
 import cv2
 import numpy as np
+import time
 
 while True:
+    begin_time = time.time()
+
     screen = pyautogui.screenshot()
     screen_arr = np.array(screen)
+    screen_arr = screen_arr[27:890, 768:, :]
     screen_arr = cv2.cvtColor(screen_arr, cv2.COLOR_RGB2BGR)
     cv2.imshow("game capture", screen_arr)
+
+    print(time.time()-begin_time)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
