7926886b9a5460f8936fa9abced42ea36f3e46d2
0xKolyanus <mukola.1zt.@gmail.com>
2022-12-02 15:43:38 +0200

    small change

--- src/gamecap.py
+++ src/gamecap.py
@@ -6,13 +6,13 @@ import mss
 i = 0
 
 with mss.mss() as sct:
-    monitor = {"top": 0, "left": 0, "width": 600, "height": 500}
+    monitor = {"top": 26, "left": 0, "width": 800, "height": 600}
 
     i = 0
     start_time = time.time()
-    while i<500:
+    while True:
         img = np.array(sct.grab(monitor))
-        cv2.imshow("OpenCV/Numpy normal", img)
+        cv2.imshow("OpenCV", img)
         i += 1
         if cv2.waitKey(1) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
