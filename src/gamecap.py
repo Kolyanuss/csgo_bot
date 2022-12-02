167350937854ad028d05a0ddc92c6b77e4738589
Nikolay_Laptop <mukola.1zt@gmail.com>
2022-12-02 14:00:30 +0200

    add new research

--- src/gamecap.py
+++ src/gamecap.py
@@ -3,27 +3,20 @@ import numpy as np
 import time
 import mss
 
-sum_time = 0
 i = 0
 
 with mss.mss() as sct:
-    # The screen part to capture
     monitor = {"top": 0, "left": 0, "width": 600, "height": 500}
 
     i = 0
-    while "Screen capturing":
-        last_time = time.time()
-
+    start_time = time.time()
+    while i<500:
         img = np.array(sct.grab(monitor))
         cv2.imshow("OpenCV/Numpy normal", img)
-
-        mytime = time.time() - last_time
-        print(f"fps: {1 / mytime}")
         i += 1
-        sum_time += mytime
-
-        if cv2.waitKey(25) & 0xFF == ord("q"):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             break
 
-    print("avg fps is: ", 1/(sum_time/i))
+    mytime = time.time() - start_time
+    print("avg fps is: ", 1/(mytime/i))
