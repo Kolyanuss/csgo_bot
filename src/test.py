e619dfe6d7bf67af67188ee9b430b0332377ccef
0xKolyanus <mukola.1zt.@gmail.com>
2022-12-03 00:05:08 +0200

    try implement torch

--- src/test.py
+++ src/test.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 import time
 import mss
+import torch
 
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+monitor = {"top": 26, "left": 0, "width": 640, "height": 640}
 sct = mss.mss()
-monitor = {"top": 26, "left": 0, "width": 800, "height": 600}
 i = 0
 start_time = time.time()
 
@@ -12,15 +14,17 @@ while True:
     frame = np.array(sct.grab(monitor))
     # ----------
 
-    # add: YOLO
+    results = model(frame, size=640)  # includes NMS
+    results.print()
+    
+    # results
 
     # ----------
-    cv2.imshow("OpenCV", frame)
+    cv2.imshow("OpenCV", np.array(results))
     i += 1
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cv2.destroyAllWindows()
-# vs.stop()
 mytime = time.time() - start_time
 print("avg fps is: ", 1/(mytime/i))
