3ee4b4391a3bfd10ef3ce7abbf931cd42338b9aa
0xKolyanus <mukola.1zt.@gmail.com>
2022-12-02 21:48:28 +0200

    start add yolo

--- src/test.py
+++ src/test.py
@@ -12,24 +12,10 @@ while True:
     frame = np.array(sct.grab(monitor))
     # ----------
 
-    # add: use YOLO
+    # add: YOLO
 
-    # draw a box around the models and the class name
-    for ((top, right, bottom, left), name) in zip(boxes, names):
-        # rescale the face coordinates
-        top = int(top * r)
-        right = int(right * r)
-        bottom = int(bottom * r)
-        left = int(left * r)
-        # draw the predicted name on the image
-        cv2.rectangle(frame, (left, top), (right, bottom),
-            (0, 255, 0), 2)
-        y = top - 15 if top - 15 > 15 else top + 15
-        cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-            0.75, (0, 255, 0), 2)
     # ----------
-
-    # cv2.imshow("OpenCV", frame)
+    cv2.imshow("OpenCV", frame)
     i += 1
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
