4269c155a4dd8a2ade1a1e5b4f7f2ba526e5fcce
0xKolyanus <mukola.1zt.@gmail.com>
2022-12-03 14:15:52 +0200

    rename and fix

--- src/yolo_torch.py
+++ src/yolo_torch.py
@@ -4,6 +4,15 @@ import time
 import mss
 import torch
 
+
+def drawRectangles(image, dfResults):
+    for _, row in dfResults.iterrows():
+        print((row['xmin'], row['ymin']))
+        image = cv2.rectangle(
+            image, (row['xmin'], row['ymin']), (row['xmax'], row['ymax']), (255, 0, 0), 2)
+    cv2.imshow("OpenCV", image)
+
+
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 monitor = {"top": 26, "left": 0, "width": 640, "height": 640}
 sct = mss.mss()
@@ -16,11 +25,15 @@ while True:
 
     results = model(frame, size=640)  # includes NMS
     results.print()
-    
+
+    dfResults = results.pandas().xyxy[0]
+    drawRectangles(
+        frame, dfResults[['xmin', 'ymin', 'xmax', 'ymax']].astype(int))
+
     # results
 
     # ----------
-    cv2.imshow("OpenCV", results.permute(1, 2, 0))
+    # cv2.imshow("OpenCV", results)
     i += 1
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
