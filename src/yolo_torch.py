cce590f26f3bf860b969eca3eaa5328c83bd8b23
0xKolyanus <mukola.1zt.@gmail.com>
2022-12-04 16:44:05 +0200

    small fix

--- src/yolo_torch.py
+++ src/yolo_torch.py
@@ -6,9 +6,13 @@ import torch
 
 INPUT_WIDTH = 640
 INPUT_HEIGHT = 640
-SCORE_THRESHOLD = 0.2
+SCORE_THRESHOLD = 0.5
 NMS_THRESHOLD = 0.4
 CONFIDENCE_THRESHOLD = 0.4
+# Text parameters.
+FONT_FACE = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 0.7
+THICKNESS = 1
 
 
 def drawRectangles(image, dfResults):
@@ -17,31 +21,36 @@ def drawRectangles(image, dfResults):
         image = cv2.rectangle(image, (row['xmin'], row['ymin']), (row['xmax'], row['ymax']), (255, 0, 0), 2)
     # cv2.imshow("OpenCV", image)
 
+def draw_label(im, label, x, y, color):
+    """Draw text onto image at location."""
+    # Get text size.
+    text_size = cv2.getTextSize(label, FONT_FACE, FONT_SCALE, THICKNESS)
+    dim, baseline = text_size[0], text_size[1]
+    # Use text size to create a BLACK rectangle.
+    cv2.rectangle(im, (x,y), (x + dim[0], y + dim[1] + baseline), (0,0,0), cv2.FILLED)
+    # Display text inside the rectangle.
+    cv2.putText(im, label, (x, y + dim[1]), FONT_FACE, FONT_SCALE, color, THICKNESS, cv2.LINE_AA)
 
-def wrap_detection(input_image, output_data):
+
+def draw_wrap_detection(input_image, outputs_2darr):
     class_ids = []
     confidences = []
     boxes = []
 
     image_width, image_height, _ = input_image.shape
-
     x_factor = image_width / INPUT_WIDTH
     y_factor = image_height / INPUT_HEIGHT
 
-    for row in output_data:
+    for row in outputs_2darr:
         confidence = row[4]
-        if confidence >= 0.4:
-
+        if confidence >= CONFIDENCE_THRESHOLD:
             classes_scores = row[5:]
             class_id = np.argmax(classes_scores)
-            confidence = classes_scores[class_id]
-            if (confidence > .25):
-
+            if (classes_scores[class_id] > SCORE_THRESHOLD):
                 confidences.append(confidence)
-
                 class_ids.append(class_id)
 
-                x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item() 
+                x, y, w, h = row[0], row[1], row[2], row[3]
                 left = int((x - 0.5 * w) * x_factor)
                 top = int((y - 0.5 * h) * y_factor)
                 width = int(w * x_factor)
@@ -49,18 +58,21 @@ def wrap_detection(input_image, output_data):
                 box = np.array([left, top, width, height])
                 boxes.append(box)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45)
+    # indexes = cv2.dnn.NMSBoxes(boxes, confidences, 
+    #         CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
 
-    result_class_ids = []
-    result_confidences = []
-    result_boxes = []
-
-    for i in indexes:
-        result_confidences.append(confidences[i])
-        result_class_ids.append(class_ids[i])
-        result_boxes.append(boxes[i])
-
-    return result_class_ids, result_confidences, result_boxes
+    for i in range(len(boxes)):
+        box = boxes[i]
+        left,top,width,height = box[0],box[1],box[2],box[3]
+        color = colors[int(class_ids[i]) % len(colors)]
+        # Draw bounding box.
+        #  cv2.rectangle(frame, box, color, 2)
+        cv2.rectangle(frame, (left, top), (left + width, top + height), color, THICKNESS*3)
+        # cv2.putText(frame, class_list[class_ids[i]], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+        # Class label.                      
+        label = "{}:{:.2f}".format(class_list[class_ids[i]], confidences[i])             
+        # Draw label.  
+        draw_label(input_image, label, left, top, color)
 
 
 def format_yolov5(frame):
@@ -72,8 +84,9 @@ def format_yolov5(frame):
 
 
 # model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-# model = torch.hub.load("ultralytics/yolov5", 'custom', path='config_files/yolov5s.pt')
-model = torch.hub.load("WongKinYiu/yolov7", 'yolov7', force_reload=True)
+model = torch.hub.load("ultralytics/yolov5", 'custom', path='config_files/yolov5s.pt')
+# model = torch.hub.load("WongKinYiu/yolov7", 'yolov7', force_reload=True)
+# model = torch.hub.load("WongKinYiu/yolov7", 'custom', path='config_files/yolov7.pt')
 class_list = []
 with open("config_files/classes.txt", "r") as f:
     class_list = [cname.strip() for cname in f.readlines()]
@@ -83,26 +96,21 @@ monitor = {"top": 26, "left": 0, "width": 640, "height": 640}
 i = 0
 start_time = time.time()
 while True:
+    i += 1
     frame = np.array(mss.mss().grab(monitor))
     # frame = format_yolov5(frame)  # todo: check
     results = model(frame, size=INPUT_WIDTH)  # includes NMS
     results.print()  # can be hide
-
+    
     # draw boxes variant 1
-    dfResults = results.pandas().xyxy[0]
-    drawRectangles(frame, dfResults[['xmin', 'ymin', 'xmax', 'ymax']].astype(int))
+    # dfResults = results.pandas().xyxy[0]
+    # drawRectangles(frame, dfResults[['xmin', 'ymin', 'xmax', 'ymax']].astype(int))
 
     # draw boxes variant 2
-    # class_ids, confidences, boxes = wrap_detection(frame, results.xyxy[0]) # trouble with dimentions
-    # for (classid, confidence, box) in zip(class_ids, confidences, boxes):
-    #      color = colors[int(classid) % len(colors)]
-    #      cv2.rectangle(frame, box, color, 2)
-    #      cv2.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
-    #      cv2.putText(frame, class_list[classid], (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, .5, (0,0,0))
+    draw_wrap_detection(frame, results.xyxy[0].cpu().numpy())
 
     cv2.imshow("OpenCV", frame)
     # ending
-    i += 1
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
