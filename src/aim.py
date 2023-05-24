77b4e8b369b9739e32e9f362cab7550f40c01dd4
0xKolyanus <mukola.1zt.@gmail.com>
2023-05-24 17:10:46 +0300

    to aim.py added execution time calc, use new mouse lib 4 move, and new ctypes lib 4 sleep

--- src/aim.py
+++ src/aim.py
@@ -1,18 +1,30 @@
 from windmouse import wind_mouse
-import pyautogui
+import mouse
+import time
+import ctypes
+
+def sleep_milliseconds(milliseconds):
+    # Виклик функції time.sleep() з модуля kernel32
+    ctypes.windll.kernel32.Sleep(milliseconds)
+
 
 def mouse_move_to(x,y):
-    pyautogui.moveTo(x, y)
+    mouse.move(x, y)
+    sleep_milliseconds(1) # не працює нище 10мс
     
 def aim(target_x,target_y):
+    start_time = time.time()
     # Отримання поточного положення курсора
-    current_x, current_y = pyautogui.position()
+    current_x, current_y = mouse.get_position()
     print("Поточне положення курсора:", current_x, current_y)
     
     # Пересування курсора до нових координат з використанням алгоритму windmouse
     wind_mouse(current_x,current_y,target_x,target_y,move_mouse=mouse_move_to)
 
     # Отримання оновленого положення курсора
-    updated_x, updated_y = pyautogui.position()
+    updated_x, updated_y = mouse.get_position()
     print("Оновлене положення курсора:", updated_x, updated_y)
+    
+    execution_time = time.time() - start_time
+    print("----Час прицілювання: ", execution_time, "секунд")
 
